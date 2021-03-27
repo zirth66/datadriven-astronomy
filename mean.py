59cc5aee99ed76a4290042d5254a6c3f506f3c48
Zirth Starfighter <zirth66@gmail.com>
2021-03-27 18:47:26 +0100

    NumPy arrays and file I/O

---
+++ mean.py
@@ -0,0 +1,7 @@
+from statistics import mean
+
+def calculate_mean(val):
+    return mean(val)
+
+mean = calculate_mean([1,2.2,0.3,3.4,7.9])
+print(mean)
---
+++ mean_manual.py
@@ -0,0 +1,5 @@
+def calculate_mean(val):
+    return sum(val) / len(val)
+
+mean = calculate_mean([1,2.2,0.3,3.4,7.9])
+print(mean)
